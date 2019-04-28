e18cb18d90266d7f9735539be22c7df36e75e864
Leigh <reposefulGrass@gmail.com>
2019-04-28 13:09:08 -0500

    Added board_initilize; Changed board_create();

--- board.c
+++ board.c
@@ -21,7 +21,6 @@ typedef enum Color {
     NEITHER, WHITE, BLACK
 } Color;
 
-
 typedef struct Piece {
     Player owner;
     PieceType type;   
@@ -40,21 +39,75 @@ char *piece_type_map[] = {
 };
 
 
+#define board_at(alpha, numeral) \
+    board[64 - numeral * 8 + (alpha - 'A')]
+
+
+Piece *
+piece_create (Player owner, PieceType type, Color color) {
+    Piece *piece = (Piece *) malloc(sizeof(Piece));
+    if (piece == NULL) {
+        fprintf(stderr, "@ %d | Unable to allocate piece!", __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    *piece = (Piece) {
+        .owner = owner,
+        .type = type,
+        .color = color
+    };
+
+    return piece;
+}
+
+
+void
+board_initialize (Piece **board, Color human) {
+    Color computer = (human == WHITE)? BLACK : WHITE;
+
+    board_at('A', 8) = piece_create(COMPUTER, ROOK,   computer); 
+    board_at('B', 8) = piece_create(COMPUTER, KNIGHT, computer);
+    board_at('C', 8) = piece_create(COMPUTER, BISHOP, computer);
+    board_at('D', 8) = piece_create(COMPUTER, KING,   computer);
+    board_at('E', 8) = piece_create(COMPUTER, QUEEN,  computer);
+    board_at('F', 8) = piece_create(COMPUTER, BISHOP, computer);
+    board_at('G', 8) = piece_create(COMPUTER, KNIGHT, computer);
+    board_at('H', 8) = piece_create(COMPUTER, ROOK,   computer);
+
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        board_at('A' + i, 7) = piece_create(COMPUTER, PAWN, computer);
+    }
+
+    for (int i = 6; i > 2; i--) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            board_at('A' + j, i) = piece_create(GAMEMASTER, NONE, NEITHER);
+        }
+    }
+
+    board_at('A', 1) = piece_create(HUMAN, ROOK,   human); 
+    board_at('B', 1) = piece_create(HUMAN, KNIGHT, human);
+    board_at('C', 1) = piece_create(HUMAN, BISHOP, human);
+    board_at('D', 1) = piece_create(HUMAN, KING,   human);
+    board_at('E', 1) = piece_create(HUMAN, QUEEN,  human);
+    board_at('F', 1) = piece_create(HUMAN, BISHOP, human);
+    board_at('G', 1) = piece_create(HUMAN, KNIGHT, human);
+    board_at('H', 1) = piece_create(HUMAN, ROOK,   human);
+
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        board_at('A' + i, 2) = piece_create(COMPUTER, PAWN, computer);
+    }
+}
+
+
 Piece **
 board_create () {
     Piece **board = (Piece **) malloc(sizeof(Piece *) * BOARD_SIZE * BOARD_SIZE);
     if (board == NULL) {
-        fprintf(stderr, "@ %d | Unable to initialize board!", __LINE__);
+        fprintf(stderr, "@ %d | Unable to allocate board!", __LINE__);
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
-        board[i] = (Piece *) malloc(sizeof(Piece));
-        if (board[i] == NULL) {
-            fprintf(stderr, "@ %d | Unable to initialize piece!", __LINE__);
-            exit(EXIT_FAILURE);
-        }
-    }
+    board_initialize(board, WHITE); // WHITE is only temp, outsource to board_create()
 
     return board;
 }
@@ -85,15 +138,6 @@ board_print (Piece **board) {
 int
 main () {
     Piece **board = board_create();
-
-    Piece *temp = (Piece *) malloc(sizeof(Piece));
-    *temp = (Piece) {
-        .owner = HUMAN,
-        .type = KING,
-        .color = BLACK
-    };
-
-    board[3 * BOARD_SIZE + 3] = temp;
 
     board_print(board);
 
